a58688a92b639056c2a2a4cdbd1959e468801a21
WelshJoeyy <welshjoeyy@gmail.com>
2015-03-11 18:22:12 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+	<title>Find a players Information</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+	<script src="js/npm.js"></script>
+<style>
+body {
+	background-image: url(grad.jpg);
+	background-position: bottom;
+	background-repeat: no-repeat;
+	background-size: 100% 500px;
+	min-width: 1320px;
+}
+#content {
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 10em;
+	width: 65%;
+}
+img.logo {
+	margin-right: auto;
+	margin-left: auto;
+	display: block;
+	width: auto;
+	height: auto;
+}
+.footer {
+	text-align: center;
+}
+</style>
+</head>
+<body>
+
+
+
+<div id="content">
+
+<div class="col-md-12">
+	<img class="logo" src="logo.png">
+</div>
+
+<div class="col-md-12">
+<form action="php.php" method="POST">
+  <div class="form-group">
+    <label for="username">Username:</label>
+    <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</div>
+
+<div class="footer">
+	<span>Created by _scrunch</span> &#8226;
+	<span>&copy;2015</span> &#8226;
+	<span>Find me on <a href="http://www.planetminecraft.com/member/tacolover22/" target="_blank">PMC</a></span>
+</div>
+
+</div>
+</body>
+</html>
