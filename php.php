8dbe0300d3fb69a5e43f9805db18cd28d5ac8cf8
WelshJoeyy <welshjoeyy@gmail.com>
2015-03-13 22:31:03 +0000

    Added so that Usernames will show in URL

--- php.php
+++ php.php
@@ -2,7 +2,7 @@
 //error_reporting(E_ALL & ~E_NOTICE);
 // Load the username from somewhere
 if (
-$username = $_POST["username"]
+$username = $_GET["username"]
 ) {
 	//do nothing 
 } else {
@@ -174,6 +174,13 @@ p.responsive {
 	<span>Created by _scrunch</span> &#8226;
 	<span>&copy;2015</span> &#8226;
 	<span>Find me on <a href="http://www.planetminecraft.com/member/tacolover22/" target="_blank">PMC</a></span>
+
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="Y8MWQB9FCUTFJ">
+<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
 
 </div>
 </div>
